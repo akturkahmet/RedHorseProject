9a60d8030f51a5b61e59fb85ad8a097ecfe65fcd
aktur <aktur@AKTURK>
2024-10-26 01:13:05 +0300

    Admin tarafının front-endi eklendi

--- RedHorseProject/Controllers/HomeController.cs
+++ RedHorseProject/Controllers/HomeController.cs
@@ -15,18 +15,22 @@ namespace RedHorseProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+        public ActionResult Rezervation()
+        {    
+          return View();
         }
         public ActionResult Test()
+        {
+            return View();
+        }
+        public ActionResult CustomerCreator()
+        {
+            return View();
+        }
+        public ActionResult Appeals()
         {
             return View();
         }
